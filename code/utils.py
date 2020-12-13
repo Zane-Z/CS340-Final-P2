a1a754ee795e52589cb435d9a6a66a4ab75e72ff
unknown <squidrog@gmail.com>
2020-12-13 13:25:34 -0800

    fixed flaten_csv

--- code/utils.py
+++ code/utils.py
@@ -13,6 +13,6 @@ def flatten_csv(path, nfiles):
         f = open(path + str(i) + ".csv", "rb")
         orig = pd.read_csv(f, header=0, sep=', ', usecols=["x0","y0","x1","y1","x2","y2","x3","y3","x4","y4","x5","y5","x6","y6","x7","y7","x8","y8","x9","y9"])
         row = orig.to_numpy().flatten()
-        fl = row
+        fl[i] = row
         f.close()
     return fl
