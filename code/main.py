af211ea3cc737a1921cde401f0247279687bebb4
unknown <squidrog@gmail.com>
2020-12-13 15:30:44 -0800

    stash formatting

--- code/main.py
+++ code/main.py
@@ -7,6 +7,7 @@ from sklearn.metrics import mean_squared_error
 
 # from sklearn.preprocessing import LabelBinarizer
 import numpy as np
+import pandas as pd
 
 if __name__ == "__main__":
 
@@ -34,6 +35,32 @@ if __name__ == "__main__":
     print("Fitting took %d seconds" % (time.time()-t))
 
     # Comput training error
-    yhat = model.predict(Xtr)
-    trainError = np.sqrt(mean_squared_error(ytr.flatten(),yhat.flatten()))
-    print("Training error = ", trainError)
+
+    # yhat = model.predict(Xtr)
+    # trainError = np.sqrt(mean_squared_error(ytr.flatten(),yhat.flatten()))
+    # print("Training error = ", trainError)
+
+    # NXval = 523
+    # pathXval = path + "val/X/X_"
+    # Xval = utils.flatten_csv_X(pathXval, NXval)
+
+    # yhat_val = model.predict(Xval)
+
+    # pathyval = path + "val/y/y_"
+    # yval = utils.flatten_csv_y(pathyval,NXval)
+
+    # valError = np.sqrt(mean_squared_error(yval.flatten(),yhat_val.flatten()))
+    # print("Validation error = ", valError)
+
+    NXtest = 19
+    pathXtest = path + "test/X"
+    Xtest = utils.flatten_csv_X(pathXtest, NXtest)
+    ypred = model.predict(Xtest)
+    fl_ytest = ypred.flatten()
+    labels = [""]*NXtest
+    ids = [""]*NXtest*60
+    for i in range(NXtest + 1):
+        for j in range(1,31):
+            idx = str(i) + "_x_" + str(j)
+            idy = str(i) + "_y_" + str(j)
+    
