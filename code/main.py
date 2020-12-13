fixed neural net to work with numerical prediction

code/main.py
@@ -13,25 +13,30 @@ if __name__ == "__main__":
     f.close()
 
     pathXtr = path + "train/X/X_"
-    NXtr = 10
+    NXtr = 100
     Xtr = utils.flatten_csv_X(pathXtr, NXtr)
 
     pathytr = path + "train/y/y_"
     Nytr = NXtr
     ytr = utils.flatten_csv_y(pathytr,Nytr)
-    Y = ytr
-    # binarizer = LabelBinarizer()
-    # Y = binarizer.fit_transform(ytr)
-
     hidden_layer_sizes = [220]
     model = NeuralNet(hidden_layer_sizes)
 
     t = time.time()
-    model.fit(Xtr,Y)
-    # model.fit_sgd(X,Y)
+    model.fit(Xtr,ytr)
     print("Fitting took %d seconds" % (time.time()-t))
 
     # Comput training error
     yhat = model.predict(Xtr)
-    trainError = np.mean(yhat != ytr)
-    print("Training error = ", trainError)
+
+    # print("ytr", ytr)
+    # print ("yhat", yhat)
+    # print (yhat.shape)
+
+    print("Comparing the first 5 x and y coords:")
+    for i in range(5):
+        print("real x and y:", ytr[0][i*2], ytr[0][i*2 + 1])
+        print("pred x and y:", yhat[0][i*2], yhat[0][i*2 + 1])
+
+    sse = np.sum((yhat - ytr)**2)
+    print ("Sum of squared error", sse)

code/neural_net.py
@@ -45,14 +45,14 @@ class NeuralNet():
 
         yhat = Z
         
-        if self.classification: # softmax- TODO: use logsumexp trick to avoid overflow
-            tmp = np.sum(np.exp(yhat), axis=1)
-            # f = -np.sum(yhat[y.astype(bool)] - np.log(tmp))
-            f = -np.sum(yhat[y.astype(bool)] - log_sum_exp(yhat))
-            grad = np.exp(yhat) / tmp[:,None] - y
-        else:  # L2 loss
-            f = 0.5*np.sum((yhat-y)**2)  
-            grad = yhat-y # gradient for L2 loss
+        # if self.classification: # softmax- TODO: use logsumexp trick to avoid overflow
+        #     tmp = np.sum(np.exp(yhat), axis=1)
+        #     # f = -np.sum(yhat[y.astype(bool)] - np.log(tmp))
+        #     f = -np.sum(yhat[y.astype(bool)] - log_sum_exp(yhat))
+        #     grad = np.exp(yhat) / tmp[:,None] - y
+        # else:  # L2 loss
+        f = 0.5*np.sum((yhat-y)**2)  
+        grad = yhat-y # gradient for L2 loss
 
         grad_W = grad.T @ activations[-2]
         grad_b = np.sum(grad, axis=0)
@@ -137,10 +137,10 @@ class NeuralNet():
         for W, b in self.weights:
             Z = X @ W.T + b
             X = 1/(1+np.exp(-Z))
-        if self.classification:
-            return np.argmax(Z,axis=1)
-        else:
-            return Z
+        # if self.classification:
+        #     return np.argmax(Z,axis=1)
+        # else:
+        return Z
 
 
 