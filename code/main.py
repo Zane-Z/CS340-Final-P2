0c19d8765b3a56577ce43191ef79a6d9f409a496
unknown <squidrog@gmail.com>
2020-12-13 13:12:11 -0800

    generalized flatten_csv

--- code/main.py
+++ code/main.py
@@ -3,9 +3,10 @@ import utils
 
 if __name__ == "__main__":
 
-    f = open("../path_to_data.txt", "r")
+    f = open(r"../path_to_data.txt", "r")
     path = f.read()
     f.close()
+    path = path + "train/X/X_"
 
     Ntr = 2307
     print(utils.flatten_csv(path, Ntr))
--- code/utils.py
+++ code/utils.py
@@ -11,7 +11,7 @@ def flatten_csv(path, nfiles):
     fl = np.zeros((nfiles+1,220))
 
     for i in range(0,nfiles + 1):
-        f = open(path + "train/X/X_" + str(i) + ".csv", "rb")
+        f = open(path + str(i) + ".csv", "rb")
         orig = pd.read_csv(f, header=0, sep=', ', usecols=["x0","y0","x1","y1","x2","y2","x3","y3","x4","y4","x5","y5","x6","y6","x7","y7","x8","y8","x9","y9"])
         row = orig.to_numpy().flatten()
         fl = row
