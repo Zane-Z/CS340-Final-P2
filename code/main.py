75fe00305ca066b1e542ff4ff794dc98eaa74884
unknown <squidrog@gmail.com>
2020-12-13 14:16:02 -0800

    CNN trial

--- code/main.py
+++ code/main.py
@@ -1,12 +1,36 @@
 import os
 import utils
+import time
+from neural_net import NeuralNet
+
+# from sklearn.preprocessing import LabelBinarizer
 
 if __name__ == "__main__":
 
     f = open(r"../path_to_data.txt", "r")
     path = f.read()
     f.close()
-    pathXtr = path + "train/X/X_"
 
-    NXtr = 2307
-    print(utils.flatten_csv(pathXtr, NXtr)[0])
+    pathXtr = path + "train/X/X_"
+    NXtr = 10
+    Xtr = utils.flatten_csv_X(pathXtr, NXtr)
+
+    pathytr = path + "train/y/y_"
+    Nytr = NXtr
+    ytr = utils.flatten_csv_y(pathytr,Nytr)
+    Y = ytr
+    # binarizer = LabelBinarizer()
+    # Y = binarizer.fit_transform(ytr)
+
+    hidden_layer_sizes = [220]
+    model = NeuralNet(hidden_layer_sizes)
+
+    t = time.time()
+    model.fit(Xtr,Y)
+    # model.fit_sgd(X,Y)
+    print("Fitting took %d seconds" % (time.time()-t))
+
+    # Comput training error
+    yhat = model.predict(Xtr)
+    trainError = np.mean(yhat != ytr)
+    print("Training error = ", trainError)
